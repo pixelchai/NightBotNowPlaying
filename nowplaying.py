6c7276582a88af78583bf590dcbb2e475e4e4b32
pixelzerg <pixelzerg@gmail.com>
2020-04-25 15:30:23 +0100

    begin implement authorisation

--- nowplaying.py
+++ nowplaying.py
@@ -1,12 +1,15 @@
 import os
 import json
 import time
+import urllib.parse
+import webbrowser
 from typing import Optional
-
 from NightPy.nightpy import NightPy
 
 class Client:
-    def __init__(self, scope="song_requests song_requests_queue"):
+    AUTH_URI = "https://api.nightbot.tv/oauth2/authorize"
+
+    def __init__(self):
         # get auth token
         auth_token = self.get_token()
         if auth_token is None:
@@ -15,7 +18,23 @@ class Client:
         self.np = NightPy(auth_token)
 
     def _authorize(self, auth_data):
-        pass
+        try:
+            params = {
+                'client_id': auth_data['client_id'],
+                'client_secret': auth_data['client_secret'],
+                'scope': 'song_requests song_requests_queue',
+                'redirect_uri': 'https://localhost',
+                'response_type': 'code'
+            }
+        except KeyError:
+            print("Your auth.json file is likely corrupted.")
+            return None
+
+        # params_escaped = {
+        #     param_name: urllib.parse.quote(params[param])
+        #     for param_name, param in params.items()
+        # }
+        webbrowser.open(Client.AUTH_URI + "?" + urllib.parse.urlencode(params))
 
     def _refresh(self, auth_data):
         pass  # todo
@@ -29,7 +48,7 @@ class Client:
 
         if 'access_token' not in auth_data:
             # access token does not exist, so must authorize
-            self._authorize(auth_data)
+            return self._authorize(auth_data)
         else:  # access token exists
             try:
                 expire_timestamp = auth_data['expire_timestamp']
@@ -37,7 +56,7 @@ class Client:
                     # maybe implement the actual refresh protocol here.
                     # for now, just reauthorize instead (which should also work fine)
                     print("Auth expired! Need to reauthorize...")
-                    self._authorize(auth_data)
+                    return self._authorize(auth_data)
                 else:
                     # not expired
                     return auth_data['access_token']
